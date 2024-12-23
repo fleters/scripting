08c70db00a7bd6f644fd3f7f73426dfab224510e
Fleter <69610510+fleters@users.noreply.github.com>
2024-12-23 19:48:21 +0700

    fix wmic not found in windows 11 24h2

--- activation-windows-10-11/activate.cmd
+++ activation-windows-10-11/activate.cmd
@@ -14,7 +14,6 @@ if %errorlevel% equ 0 (
 )
 
 :checkinternet
-@echo off
 cls & echo Fleter Project & echo =============== & echo. & echo.
 echo Mengecek Koneksi Internet...
 PING -n 1 kms8.msguides.com >NUL
@@ -32,6 +31,7 @@ IF %ERRORLEVEL% NEQ 1 (
 cls & echo Fleter Project & echo =============== & echo. & echo.
 echo Kami akan mengecek kompatibilitas sistem operasi anda.
 echo Lanjutkan? (y/n) (kosong: y)
+
 set /p tempvar=
 if [%tempvar%] == [] (
   cls
@@ -52,13 +52,19 @@ if [%tempvar%] == [] (
 
 :systeminfo
 cls & echo Fleter Project & echo =============== & echo. & echo.
-wmic os get caption | find /I "Windows 10" >nul
+set wmicpath=C:\Windows\System32\wbem\WMIC.exe
+%wmicpath% os get caption
+if [%errorlevel%] == [9009] (
+  cls
+  goto :notavaiable
+)
+%wmicpath% os get caption | find /I "Windows 10" >nul
 if [%errorlevel%] == [0] (
   echo Supported Windows 10
   timeout /t 1 >nul
   goto :confirmclear
 )
-wmic os get caption | find /I "Windows 11" >nul
+%wmicpath% os get caption | find /I "Windows 11" >nul
 if [%errorlevel%] == [0] (
   echo Supported Windows 11
   timeout /t 1 >nul
@@ -100,55 +106,55 @@ cls & echo Fleter Project & echo =============== & echo. & echo.
 echo Mengaktivasi Windows Anda...
 echo.
 timeout /t 2 /nobreak >nul
-wmic os get caption | find /I "Pro N for Workstations" >nul
+%wmicpath% os get caption | find /I "Pro N for Workstations" >nul
 if [%errorlevel%] == [0] (
   goto :notavaiable
 )
-wmic os get caption | find /I "Pro for Workstations" >nul
+%wmicpath% os get caption | find /I "Pro for Workstations" >nul
 if [%errorlevel%] == [0] (
   set key=NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
   goto :msgang-script
 )
-wmic os get caption | find /I "Pro Education N" >nul
+%wmicpath% os get caption | find /I "Pro Education N" >nul
 if [%errorlevel%] == [0] (
   goto :notavaiable
 )
-wmic os get caption | find /I "Pro Education" >nul
+%wmicpath% os get caption | find /I "Pro Education" >nul
 if [%errorlevel%] == [0] (
   set key=6TP4R-GNPTD-KYYHQ-7B7DP-J447Y
   goto :msgang-script
 )
-wmic os get caption | find /I "Pro N" >nul
+%wmicpath% os get caption | find /I "Pro N" >nul
 if [%errorlevel%] == [0] (
   set key=MH37W-N47XK-V7XM9-C7227-GCQG9
   goto :msguides-script
 )
-wmic os get caption | find /I "Pro" >nul
+%wmicpath% os get caption | find /I "Pro" >nul
 if [%errorlevel%] == [0] (
   set key=W269N-WFGWX-YVC9B-4J6C9-T83GX
   goto :msguides-script
 )
-wmic os get caption | find /I "Education N" >nul
+%wmicpath% os get caption | find /I "Education N" >nul
 if [%errorlevel%] == [0] (
   set key=2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
   goto :msguides-script
 )
-wmic os get caption | find /I "Education" >nul
+%wmicpath% os get caption | find /I "Education" >nul
 if [%errorlevel%] == [0] (
   set key=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
   goto :msguides-script
 )
-wmic os get caption | find /I "Home Single Language" >nul
+%wmicpath% os get caption | find /I "Home Single Language" >nul
 if [%errorlevel%] == [0] (
   set key=7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
   goto :msguides-script
 )
-wmic os get caption | find /I "Home N" >nul
+%wmicpath% os get caption | find /I "Home N" >nul
 if [%errorlevel%] == [0] (
   set key=3KHY7-WNT83-DGQKR-F7HPR-844BM
   goto :msguides-script
 )
-wmic os get caption | find /I "Home" >nul
+%wmicpath% os get caption | find /I "Home" >nul
 if [%errorlevel%] == [0] (
   set key=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
   goto :msguides-script
